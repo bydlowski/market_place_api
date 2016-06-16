76f1bb7fd9bffa4742cb6405ece10771c3d93742
Pedro <pedro@bydlowski.com>
2016-06-16 10:44:54 -0700

    Adds shoulda matchers for spec

--- lib/spec/models/user_spec.rb
+++ lib/spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+it { should validate_presence_of(:email) }
+it { should validate_uniqueness_of(:email) }
+it { should validate_confirmation_of(:password) }
+it { should allow_value('example@domain.com').for(:email) }
+
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
+end
+
 describe User do
   before { @user = FactoryGirl.build(:user) }
 
